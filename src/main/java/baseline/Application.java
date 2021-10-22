6ffefd10d47736cf5e0df5af86d9324fbce1d5d2
Alexys Veloz <alexys.veloz.fl@gmail.com>
2021-10-22 00:02:11 -0400

    all the fxml files I think I need

---
+++ src/main/java/baseline/Application.java
@@ -0,0 +1,29 @@
+package baseline;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+
+public class Application extends javafx.application.Application {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Lister");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application myApplication = new Application();
+        launch(args);
+    }
+
+}
